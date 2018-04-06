166735d909fda4ca2839c9d34b8494a44bd16569
julian1 <julian.atkinson71@gmail.com>
2018-04-06 19:37:08 +1000

    initial aeson

--- cabal-project/src/Config.hs
+++ cabal-project/src/Config.hs
@@ -4,8 +4,14 @@ module Config where
 import qualified Database.PostgreSQL.Simple as PG( defaultConnectInfo, ConnectInfo(..))
 
 
+import Data.Aeson
+
+-- type MyConnectInfo = PG.ConnectInfo
+--    deriving (Show, Eq)
+-- can we write aeson functions
+
 connectionInfo :: PG.ConnectInfo
-connectionInfo = PG.defaultConnectInfo { 
+connectionInfo = PG.defaultConnectInfo {
       PG.connectHost = "postgres.localnet2"
     , PG.connectPort = 5432
     , PG.connectDatabase = "test"
@@ -13,5 +19,20 @@ connectionInfo = PG.defaultConnectInfo {
     , PG.connectPassword = "test"
 }
 
+
+
+instance ToJSON PG.ConnectInfo
+  where toJSON (PG.ConnectInfo a b c d e )   =  object [] 
+
+-- deriving (Show, Eq, Generic, ToJSON, FromJSON) -- deriving (Show, Eq)
 -- poolInfo
 
+{-
+instance ToJSON Coord where
+  toJSON (Coord xV yV) = object [ "x" .= xV,
+                                  "y" .= yV ]
+
+  toEncoding Coord{..} = pairs $
+    "x" .= x <>
+  "y" .= y
+-}
